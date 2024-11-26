f4103c145801fa101ca5911a85551b79609bc321
vishnu <vishnulodhia@gmail.com>
2024-11-26 23:38:55 +0530

    create a custom validation

---
+++ src/test/java/com/springBootLearning/spring_boot_0_1/SpringBoot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootLearning.spring_boot_0_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
